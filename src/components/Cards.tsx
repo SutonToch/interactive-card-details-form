abd54bf5199a4742d6d9252aea7d808d419eeb22
SutonToch <77286229+SutonToch@users.noreply.github.com>
2023-12-14 15:04:06 +0100

    finalize cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,17 +3,28 @@ import cardFront from './../assets/bg-card-front.png'
 import cardFrontLogo from './../assets/card-logo.svg'
 import cardBack from './../assets/bg-card-back.png'
 
-export default function Cards() {
+interface CardsProps {
+  name:string,
+  cardNumber:string,
+  month:string,
+  year:string,
+  cvc:string
+}
+
+export default function Cards(props:CardsProps) {
 
   return (
     <>
         <div className="card-front-container">
             <img src={cardFront} />
             <img className="card-logo" src={cardFrontLogo} />
+            <p className="card-front-cardNumber">{props.cardNumber ? props.cardNumber : "0000 0000 0000 0000"}</p>
+            <p className="card-front-name">{props.name ? props.name : "Jane Appleseed"}</p>
+            <p className="card-front-month-year">{props.month ? props.month : "00"}/{props.year ? props.year : "00"}</p>
         </div>
         <div className="card-back-container">
             <img src={cardBack}/>
-            <p className="card-back-cvc"></p>
+            <p className="card-back-cvc">{props.cvc ? props.cvc : "000"}</p>
         </div>
     </>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,34 +3,73 @@ import Cards from './Cards.tsx'
 import './../styles/Form.css'
 
 export default function Form() {
-
-    const [cvc, setCvc] = useState("000");
-
+    const [name, setName] = useState("");
+    const [cardNumber, setCardNumber] = useState("");
+    const [month, setMonth] = useState("");
+    const [year, setYear] = useState("");
+    const [cvc, setCvc] = useState("");
+    
     return (
         <>
             <form>
                 <div className="form-box">
                     <label htmlFor={"name"}>Cardholder Name</label>
-                    <input placeholder={"e.g. Jane Appleseed"} id={"name"} name={"name"}/>
+                    <input
+                        placeholder={"e.g. Jane Appleseed"}
+                        value={name}
+                        id={"name"} 
+                        name={"name"}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                        
                 </div>
                 <div className="form-box">
                     <label htmlFor={"card_number"}>Card Number</label>
-                    <input placeholder={"e.g. 1234 5678 9123 0000"} id={"card_number"} name={"card_number"}/>
+                    <input 
+                        placeholder={"e.g. 1234 5678 9123 0000"}
+                        value={cardNumber}
+                        id={"card_number"} 
+                        name={"card_number"}
+                        onChange={(e) => setCardNumber(e.target.value)}
+                    />
                 </div>
                 <div className="form-box multi-input-line">
                     <div className="form-box-date">
                         <label htmlFor={"month"}>Exp. Date (MM/YY)</label>
-                        <input placeholder={"MM"} id={"month"} name={"month"}/>
-                        <input placeholder={"YY"} name={"year"}/>
+                        <input 
+                            placeholder={"MM"}
+                            value={month} 
+                            id={"month"} 
+                            name={"month"}
+                            onChange={(e) => setMonth(e.target.value)}
+                        />
+                        <input
+                            placeholder={"YY"}
+                            value={year}
+                            name={"year"}
+                            onChange={(e) => setYear(e.target.value)}
+                        />
                     </div>
                     <div>
                         <label htmlFor={"cvc"}>CVC</label>
-                        <input placeholder={"e.g. 123"} id={"cvc"} name={"cvc"}/>
+                        <input 
+                            placeholder={"e.g. 123"} 
+                            value={cvc}
+                            id={"cvc"} 
+                            name={"cvc"}
+                            onChange={(e) => setCvc(e.target.value)}
+                        />
                     </div>
                 </div>
                 <input className="submit" type={"submit"} value="Confirm" />
             </form>
-            <Cards />
+            <Cards
+                name={name}
+                cardNumber={cardNumber}
+                month={month}
+                year={year}
+                cvc={cvc}
+            />
         </>
     )
   }
