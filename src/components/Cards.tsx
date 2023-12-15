bff6758642e369b250a6af02dd45c5757fbd14bb
SutonToch <77286229+SutonToch@users.noreply.github.com>
2023-12-15 11:59:01 +0100

    update regex and state management

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,27 +4,31 @@ import cardFrontLogo from './../assets/card-logo.svg'
 import cardBack from './../assets/bg-card-back.png'
 
 interface CardsProps {
-  name:string,
-  cardNumber:string,
-  month:string,
-  year:string,
-  cvc:string
+  formData: {
+    name:string,
+    card_number:string,
+    month:string,
+    year:string,
+    cvc:string
+  }
 }
 
 export default function Cards(props:CardsProps) {
+
+  const formData = props.formData
 
   return (
     <>
         <div className="card-front-container">
             <img src={cardFront} />
             <img className="card-logo" src={cardFrontLogo} />
-            <p className="card-front-cardNumber">{props.cardNumber ? props.cardNumber : "0000 0000 0000 0000"}</p>
-            <p className="card-front-name">{props.name ? props.name : "Jane Appleseed"}</p>
-            <p className="card-front-month-year">{props.month ? props.month : "00"}/{props.year ? props.year : "00"}</p>
+            <p className="card-front-cardNumber">{formData.card_number ? formData.card_number : "0000 0000 0000 0000"}</p>
+            <p className="card-front-name">{formData.name ? formData.name : "Jane Appleseed"}</p>
+            <p className="card-front-month-year">{formData.month ? formData.month : "00"}/{formData.year ? formData.year : "00"}</p>
         </div>
         <div className="card-back-container">
             <img src={cardBack}/>
-            <p className="card-back-cvc">{props.cvc ? props.cvc : "000"}</p>
+            <p className="card-back-cvc">{formData.cvc ? formData.cvc : "000"}</p>
         </div>
     </>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,11 +3,31 @@ import Cards from './Cards.tsx'
 import './../styles/Form.css'
 
 export default function Form() {
-    const [name, setName] = useState("");
-    const [cardNumber, setCardNumber] = useState("");
-    const [month, setMonth] = useState("");
-    const [year, setYear] = useState("");
-    const [cvc, setCvc] = useState("");
+    const [form, setForm] = useState({
+        name: "",
+        card_number: "",
+        month: "",
+        year: "",
+        cvc: ""
+    })
+
+    function updateForm(target: HTMLInputElement) {
+        const {name, value, pattern} = target;
+        setForm((prevForm) => {
+            return (
+                {
+                    ...prevForm,
+                    [name]: value
+                }
+            )
+        })
+
+        if(!value.match(pattern)) {
+            target.classList.add("invalid")
+        } else {
+            target.classList.remove("invalid")
+        }
+    }
     
     return (
         <>
@@ -16,24 +36,24 @@ export default function Form() {
                     <label htmlFor={"name"}>Cardholder Name</label>
                     <input
                         placeholder={"e.g. Jane Appleseed"}
-                        value={name}
+                        value={form.name}
                         id={"name"} 
                         name={"name"}
-                        pattern={"[A-Za-z]+\\s+[A-Za-z\\s]+"}
+                        pattern={"^[A-Za-z]+\\s+[A-Za-z\\s]+$"}
                         required
-                        onChange={(e) => setName(e.target.value)}
+                        onChange={(e) => updateForm(e.target)}
                     />     
                 </div>
                 <div className="form-box">
                     <label htmlFor={"card_number"}>Card Number</label>
                     <input 
                         placeholder={"e.g. 1234 5678 9123 0000"}
-                        value={cardNumber}
+                        value={form.card_number}
                         id={"card_number"} 
                         name={"card_number"}
-                        pattern={"[0-9]{4}\\s[0-9]{4}\\s[0-9]{4}\\s[0-9]{4}"}
+                        pattern={"^[0-9]{4}\\s[0-9]{4}\\s[0-9]{4}\\s[0-9]{4}$"}
                         required
-                        onChange={(e) => setCardNumber(e.target.value)}
+                        onChange={(e) => updateForm(e.target)}
                     />
                 </div>
                 <div className="form-box multi-input-line">
@@ -41,43 +61,39 @@ export default function Form() {
                         <label htmlFor={"month"}>Exp. Date (MM/YY)</label>
                         <input 
                             placeholder={"MM"}
-                            value={month} 
+                            value={form.month} 
                             id={"month"} 
                             name={"month"}
-                            pattern={"01||02|03|04|05|06|07|08|09|10|11|12"}
+                            pattern={"^01|02|03|04|05|06|07|08|09|10|11|12$"}
                             required
-                            onChange={(e) => setMonth(e.target.value)}
+                            onChange={(e) => updateForm(e.target)}
                         />
                         <input
                             placeholder={"YY"}
-                            value={year}
+                            value={form.year}
                             name={"year"}
-                            pattern={"[2-9][0-9]"}
+                            pattern={"^[2-9][0-9]$"}
                             required
-                            onChange={(e) => setYear(e.target.value)}
+                            onChange={(e) => updateForm(e.target)}
                         />
                     </div>
                     <div>
                         <label htmlFor={"cvc"}>CVC</label>
                         <input 
                             placeholder={"e.g. 123"} 
-                            value={cvc}
+                            value={form.cvc}
                             id={"cvc"} 
                             name={"cvc"}
-                            pattern={"[0-9][0-9][0-9]"}
+                            pattern={"^[0-9][0-9][0-9]$"}
                             required
-                            onChange={(e) => setCvc(e.target.value)}
+                            onChange={(e) => updateForm(e.target)}
                         />
                     </div>
                 </div>
                 <input className="submit" type={"submit"} value="Confirm" />
             </form>
             <Cards
-                name={name}
-                cardNumber={cardNumber}
-                month={month}
-                year={year}
-                cvc={cvc}
+                formData={form}
             />
         </>
     )
